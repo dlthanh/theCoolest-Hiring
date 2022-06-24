c900fac2222e0a2b4dd954f24663b397607df734
dlthanh <doanlethanh.1812@gmail.com>
2022-06-24 17:54:59 +0700

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React, {FC} from "react";
+
+const Header: FC = (): JSX.Element => {
+  return (
+    <div className="header">
+      <div className="header-logo">
+        <img src="/assets/images/logo.svg" alt="theCoolest Logo"/>
+      </div>
+
+      <div className="header-nav">
+        <div className="header-nav-item">Who we are?</div>
+        <div className="header-nav-item">Vacancy</div>
+        <div className="header-nav-item">Contact</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
